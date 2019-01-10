11a5490ef4d7563677925cad21fe8bd288d949f8
Miles Adamson <milesadamson@hotmail.com>
2019-01-09 21:21:24 -0700

    Completely removed ads

--- app/src/main/java/com/adamson/miles/climbingtrainer/MainActivity.java
+++ app/src/main/java/com/adamson/miles/climbingtrainer/MainActivity.java
@@ -1,9 +1,5 @@
 package com.adamson.miles.climbingtrainer;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.MobileAds;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
