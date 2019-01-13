eaad9bc71ac7025b94ca9bf41e5e7ccfca21fa14
Miles Adamson <milesadamson@hotmail.com>
2019-01-13 02:50:06 -0700

    made description font smaller in timer counter

--- app/src/main/java/com/adamson/miles/climbingtrainer/AddExercise.java
+++ app/src/main/java/com/adamson/miles/climbingtrainer/AddExercise.java
@@ -1,7 +1,6 @@
 package com.adamson.miles.climbingtrainer;
 
 import android.content.Intent;
-import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
